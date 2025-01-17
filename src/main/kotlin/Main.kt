f114c6cbe7a9426ba8eba6bc44aabcd74caea333
ROMKA9915 <romka9915@gmail.com>
2025-01-17 18:35:42 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,34 +1,54 @@
 package ru.netology
 
-fun main() {
-    val cardType = "Visa"
-    val monthLimitMasterCard = 75_000
-    val limitOnDay = 150_000
-    val limitOnMonth = 600_000
-    val amountRemittanceAgoToday = 0
-    val amountRemittanceAgoMonth = 0
-    val amount = 150_000
+const val cardType = "MasterCard"
+const val monthLimitMasterCard = 75_000
+const val limitOnDay = 150_000
+const val limitOnMonth = 600_000
+const val amountRemittanceAgoToday = 0
+const val amountRemittanceAgoMonth = 75000
+const val amount = 10_000
 
-    fun taxRemittance(cardType: String = "Мир", amountRemittanceAgoToday: Int = 0, amountRemittanceAgoMonth: Int = 0, amount: Int) {
-        if (amountRemittanceAgoMonth + amountRemittanceAgoToday + amount >limitOnMonth || amountRemittanceAgoToday + amount > limitOnDay) {
-            return println("Превышен лимит перевода")
-        }
-        return when (cardType){
-            "MasterCard" -> if (amountRemittanceAgoMonth + amountRemittanceAgoToday + amount > monthLimitMasterCard){
-                val comission = ((amountRemittanceAgoMonth + amountRemittanceAgoToday + amount) - monthLimitMasterCard) *0.006 + 20
-                println("Сумма комиссии составит: $comission руб.")
-            } else {
-                val comission = amount * 0.006 + 20
-                println("Сумма комиссии составит: $comission руб.")
-            }
-            "Visa" -> if (amount * 0.0075 < 35) {
-                println("Сумма комиссия составит : 35 руб.")
-            } else {
-                println("Сумма комиссия составит : " + amount * 0.0075 + " руб.")
-            }
-            else -> println("Перевод без комиссии")
-        }
-    }
+fun main() {
 
     taxRemittance(cardType, amountRemittanceAgoToday, amountRemittanceAgoMonth, amount)
+
+}
+
+fun taxRemittance(
+    cardType: String = "Мир",
+    amountRemittanceAgoToday: Int = 0,
+    amountRemittanceAgoMonth: Int = 0,
+    amount: Int
+) {
+    if (amountRemittanceAgoMonth + amountRemittanceAgoToday + amount > limitOnMonth || amountRemittanceAgoToday + amount > limitOnDay) {
+        return println("Превышен лимит перевода")
+    }
+    return when (cardType) {
+        "MasterCard" -> when {
+            amount > monthLimitMasterCard -> println("Сумма комиссия составит : " + ((amount - monthLimitMasterCard) * 0.006 + 20) + " руб.")
+
+            amountRemittanceAgoToday + amountRemittanceAgoMonth + amount > monthLimitMasterCard &&
+                    amountRemittanceAgoToday + amountRemittanceAgoMonth >= monthLimitMasterCard -> println("Сумма комиссия составит : " + (amount * 0.006 + 20) + " руб.")
+
+            amountRemittanceAgoToday + amountRemittanceAgoMonth > monthLimitMasterCard -> println("Сумма комиссия составит : " + (amount * 0.006 + 20) + " руб.")
+
+            amountRemittanceAgoToday + amountRemittanceAgoMonth + amount > monthLimitMasterCard &&
+                    amountRemittanceAgoToday + amountRemittanceAgoMonth < monthLimitMasterCard -> if (amountRemittanceAgoMonth + amountRemittanceAgoToday > amount) {
+                println("Сумма комиссия составит : " + ((monthLimitMasterCard - (amountRemittanceAgoToday + amountRemittanceAgoMonth)) * 0.006 + 20) + " руб.")
+            } else {
+                println("Сумма комиссия составит : " + ((monthLimitMasterCard - amount) * 0.006 + 20) + " руб.")
+            }
+
+                else -> println("Перевод без комиссии")
+        }
+
+        "Visa" -> if (amount * 0.0075 < 35) {
+            println("Сумма комиссия составит : 35 руб.")
+        } else {
+            println("Сумма комиссия составит : " + amount * 0.0075 + " руб.")
+        }
+
+        "Мир" -> println("Перевод без комиссии")
+        else -> println("Не корректный тип карты")
+    }
 }
